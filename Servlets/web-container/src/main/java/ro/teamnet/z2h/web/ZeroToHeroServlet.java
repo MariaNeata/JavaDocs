bc8c15ab78ffd7cf1a5b0b36d91d097819381f6a
MariaNeata <maria.neata@gmail.com>
2015-05-04 18:04:01 +0300

    Servlets

--- Servlets/web-container/src/main/java/ro/teamnet/z2h/web/ZeroToHeroServlet.java
+++ Servlets/web-container/src/main/java/ro/teamnet/z2h/web/ZeroToHeroServlet.java
@@ -1,12 +1,12 @@
 /**
  * ZeroToHeroServlet.java
- *
+ * <p/>
  * Copyright (c) 2014 Teamnet. All Rights Reserved.
- *
+ * <p/>
  * This source file may not be copied, modified or redistributed,
  * in whole or in part, in any form or for any reason, without the express
  * written consent of Teamnet.
- **/
+ */
 
 package ro.teamnet.z2h.web;
 
@@ -17,10 +17,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ZeroToHeroServlet extends HttpServlet {
+    private String handleRequest(HttpServletRequest req) {
+        String response = "Hello <b>"+req.getParameter("firstName")+" "+ req.getParameter("lastName")+"</b>! Enjoy Zero To Hero!!!";
+        return response;
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO de implementat
+        resp.setContentType("text/html");
+        resp.getWriter().write(handleRequest(req));
     }
 
 }
